eb4e43b3e64c74bd1e65d6cea54020125d5242d2
Araa2876 <anibal.arango@usherbrooke.ca>
2025-09-15 20:57:09 -0400

    Docker file from GPT

---
+++ docker/ros_entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Source ROS installation
+if [ -f "/opt/ros/${ROS_DISTRO}/setup.bash" ]; then
+  source "/opt/ros/${ROS_DISTRO}/setup.bash"
+fi
+
+# If workspace overlay exists, source it too
+WS_SETUP="/home/${USERNAME}/clankus_ws/install/setup.bash"
+if [ -f "${WS_SETUP}" ]; then
+  source "${WS_SETUP}"
+fi
+
+# Execute whatever was passed to the container
+exec "$@"
